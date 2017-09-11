cac2a7b0375084fcda2f595e66b14fc826019bc5
lbuckley <l.b.buckley@gmail.com>
2017-09-11 12:15:54 -0700

    Update robomussel analysis.

--- analysis/Robomussel.R
+++ analysis/Robomussel.R
@@ -36,17 +36,37 @@ te.sd <- do.call(rbind,lapply(file_names,my.read.table))
 te.wa= rbind(te.cc, te.lb, te.cp,te.sd)
 
 #extract sites and numbers
-#te.wa$id= gsub("BMRMUSWA","",te.wa$id)
-#te.wa$site= substr(te.wa$id, 1, 2)
+te.wa$id1= gsub("BMRMUSWA","",te.wa$id)
+te.wa$site= substr(te.wa$id1, 1, 2)
+
+#extract subsite
+te.wa$subsite=  substr(te.wa$id1, 3, 4)
+te.wa$subsite= as.numeric(gsub("_","",te.wa$subsite))
+
+te.wa$date= te.wa$row.names
+
+#find daily max
+te.max= te.wa %>% group_by(date, site, subsite) %>% summarise(id1=id1[1], MaxTemp_C= max(Temp_C))
+
+day=  as.POSIXlt(te.max$date, format="%m/%d/%Y")
+te.max$doy=as.numeric(strftime(day, format = "%j"))
+te.max$j=julian(day)
 
 #----------------------
 #PLOTS
 
-BMRMUSWASD
-
-
-
-#-------------------------
 #FREQUENCY
 # https://github.com/georgebiogeekwang/tempcycles/
-library("tempcycles")
+
+#power spectrum
+#x: frequency (1/days)
+#y: log amplitude
+
+#need to incorporate within site variation
+te.dat= subset(te.max, te.max$site=="SD")
+te.max.lomb <- spec_lomb_phase(te.dat$MaxTemp_C, te.dat$doy, freq=seq(0,1,0.01))
+te.max.lomb <- spec_lomb_phase(te.dat$MaxTemp_C, te.dat$j, freq=seq(0,1,0.01))
+
+
+#freq, amp plot
+plot(te.max.lomb$freq, te.max.lomb$cyc_range/2, type="l", log="y")
