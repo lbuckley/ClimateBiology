43b00744a4911e3deae240cd6a8e9d348d79734e
lbuckley <l.b.buckley@gmail.com>
2017-10-11 12:18:26 -0700

    Update biotraits analysis

--- analysis/BioTraits.R
+++ analysis/BioTraits.R
@@ -15,8 +15,10 @@ traits= read.csv("Delletal2013.csv")
 hist(traits$AmbientTemp, n=30)
 
 #FIG A
-ggplot(traits, aes(x=AmbientTemp, color=TraitOrg)) +
-  geom_histogram(fill="white", alpha=0.5, position="identity")+theme_bw()
+#fig3a<- ggplot(traits, aes(x=AmbientTemp, color=TraitOrg)) + geom_histogram(fill="white", alpha=0.5, position="identity")+theme_bw()+ theme(legend.position="none")
+
+fig3a<- ggplot(traits, aes(x=AmbientTemp, color=TraitOrg, size=1)) +
+  geom_density()+theme_bw()+ theme(legend.position="none")+ labs(x = "Measurement temperature (°C)")
 
 #stats by series
 traits2 = traits %>% group_by(DataSeriesID) %>% summarise(TempMean= mean(AmbientTemp),TempN= length(AmbientTemp),TempRange= range(AmbientTemp)[2]-range(AmbientTemp)[1], TimeMean=mean(ObsTimeValueSI),TimeUnit=ObsTimeUnitSI[1], LabField=Labfield[1] )
@@ -29,8 +31,8 @@ plot(hist1, xlim=c(0,10))
 hist(traits2$TempRange)
 
 #FIG B
-ggplot(traits3, aes(x=TempN, color=TraitOrg)) +
-  geom_histogram(fill="white", alpha=0.5, position="identity")+xlim(0,10)+theme_bw()
+fig3b<- ggplot(traits3, aes(x=TempN, color=TraitOrg)) +
+  geom_density(aes(size=1))+xlim(0,12)+theme_bw()+ labs(x = "Number measurement temperatures", colour="trait type", y="")+ guides(size=FALSE)
 
 #Time
 traits4= subset(traits2, traits2$TimeUnit=="second")
@@ -40,3 +42,26 @@ plot(hist1, xlim=c(0,10))
 #STATS IN TEXT?
 ggplot(traits4, aes(x=TimeMean/60)) +
   geom_histogram(fill="gray", alpha=0.5, position="identity")+theme_bw()
+
+#-----------------------
+#FIG 3
+
+multiplot(fig3a, fig3b, cols=2)
+cbind(fig3a, fig3b)
+
+#=========================================
+
+setwd("C:\\Users\\lbuckley\\Desktop\\Fall2017\\ICBClimBio\\")
+pdf("Fig3.pdf", height=4, width=8)
+#tiff("Fig3.tiff", res = 300) #height=400, width=800, 
+
+#plot
+grid.newpage()
+pushViewport(viewport(layout=grid.layout(1,2, widths=c(0.5,0.7) )))
+vplayout<-function(x,y)
+  viewport(layout.pos.row=x,layout.pos.col=y)
+print(fig3a,vp=vplayout(1,1))
+print(fig3b,vp=vplayout(1,2))
+
+dev.off()
+
